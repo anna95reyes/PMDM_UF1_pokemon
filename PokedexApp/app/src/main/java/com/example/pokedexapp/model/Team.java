f9e402baa7827b44e124123e7d9061bd0bdff577
Anna <areyes@milaifontanals.org>
2022-11-30 20:37:21 +0100

    30/11/2022 - Començament teams

---
+++ PokedexApp/app/src/main/java/com/example/pokedexapp/model/Team.java
@@ -0,0 +1,53 @@
+package com.example.pokedexapp.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team implements Serializable {
+
+    private final Integer MAX_POKEMONS = 6;
+
+    private String name;
+    private Boolean complert;
+    private List<Pokemon> pokemons;
+
+    public Team(String name) {
+        this.name = name;
+        this.complert = false;
+        this.pokemons = new ArrayList<Pokemon>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getComplert() {
+        return complert;
+    }
+
+    public List<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public boolean addPokemon(Pokemon pokemon) {
+        boolean afegit = false;
+        if (pokemons.size() == MAX_POKEMONS) {
+            complert = true;
+        }
+        if (!complert && !pokemons.contains(pokemon)) {
+            pokemons.add(pokemon);
+            afegit = true;
+        }
+        return afegit;
+    }
+
+    public boolean removePokemon(Pokemon pokemon) {
+        return pokemons.remove(pokemon);
+    }
+
+}
